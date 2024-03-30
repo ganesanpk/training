680accbf8f290dd527f60b4041498c60c5d449fe
Ganesan <ganesh.cse2003@gmail>
2024-03-30 21:00:26 +0530

    js task

--- sarupriya/JavaScript/script.js
+++ sarupriya/JavaScript/script.js
@@ -1,64 +1,80 @@
-var name="JavaScript";
-console.log(name);
-function myFunction() {
-    document.getElementById("demo2").innerHTML = "Paragraph changed.";
-}
-function myFunction1() {
-    document.getElementById("demo4").innerHTML = "Paragraph changed file path.";
-}
-let x, y, z;  
-x = 5;       
-y = 16;        
-z = x + y; 
-console.log(z)
-const a=7;
-console.log(a)
-//Declare a variable
-let b;
-b="files";
-console.log(b);
-// declare multiple varible in one line
-let c=15,d=20,e=78;
-console.log(c,d,e)
+// var name="JavaScript";
+// console.log(name);
+// function myFunction() {
+//     document.getElementById("demo2").innerHTML = "Paragraph changed.";
+// }
+// function myFunction1() {
+//     document.getElementById("demo4").innerHTML = "Paragraph changed file path.";
+// }
+// let x, y, z;  
+// x = 5;       
+// y = 16;        
+// z = x + y; 
+// console.log(z)
+// const a=7;
+// console.log(a)
+// //Declare a variable
+// let b;
+// b="files";
+// console.log(b);
+// // declare multiple varible in one line
+// let c=15,d=20,e=78;
+// console.log(c,d,e)
 
-let f= "5" + 2 + 3;
-console.log(f);
+// let f= "5" + 2 + 3;
+// console.log(f);
 
-let g= 2 + 6 + "5";
-console.log(g);
+// let g= 2 + 6 + "5";
+// console.log(g);
 
-let $=6;
-let $$my=78;
-console.log($+$$my);
+// let $=6;
+// let $$my=78;
+// console.log($+$$my);
 
-var h=45;
-{
-    var h=7;
-}
-console.log(h);
+// var h=45;
+// {
+//     var h=7;
+// }
+// console.log(h);
 
-//operators
-let l=5;
-let m=2;
-let n=l+m;
-console.log(n);
+// //operators
+// let l=5;
+// let m=2;
+// let n=l+m;
+// console.log(n);
 
-let l1=5;
-let m1=2;
-let n1=l-m;
-console.log(n1);
+// let l1=5;
+// let m1=2;
+// let n1=l-m;
+// console.log(n1);
 
-let l2=5;
-let m2=2;
-let n2=l*m;
-console.log(n2);
+// let l2=5;
+// let m2=2;
+// let n2=l*m;
+// console.log(n2);
 
-let l3=5;
-let m3=2;
-let n3=l/m;
-console.log(n3);
+// let l3=5;
+// let m3=2;
+// let n3=l/m;
+// console.log(n3);
 
-let l4=5;
-let m4=2;
-let n4=l%m;
-console.log(n4);
+// let l4=5;
+// let m4=2;
+// let n4=l%m;
+// console.log(n4);
+
+var dat = new Date();
+var day = dat.getDay();
+const days=['sun','monday','tuesday','wednessday','thusday','friday','saturday'];
+console.log(days[day]);
+var month = dat.getMonth();
+console.log(month);
+var dates = dat.getDate();
+console.log(dates);
+
+// output
+// 01-04-2024
+// 01 Mon - 04 April - 2024
+// give dob candidate adult or not
+// age with month
+
